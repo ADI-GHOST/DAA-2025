cd4fcbfb81f1747becee2a5f4e912666a957597e
aditya anand <aditya68523490@gmail.com>
2025-08-11 21:18:18 +0530

    Update low to up.cpp

--- daa week 1/low to up.cpp
+++ daa week 1/low to up.cpp
@@ -2,17 +2,28 @@
 #include <string>
 using namespace std;
 
-string up ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-string low ="abcdefghijklmnopqrstuvwxyz"
-string str = "my name is aditya anand"
+int main() {
+    string up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string low = "abcdefghijklmnopqrstuvwxyz";
+    string str = "my name is aditya anand";
 
-int len =0;
-while(input[len]!='\0'){
-    len=len+1;
-}
-
-for(int i=0;i<len;i++){
-    for(int j=0;j<26;j++){
-        if(str)
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
     }
+
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < 26; j++) {
+            if (str[i] == low[j]) {
+                char temp = str[i];
+                str[i] = up[j];
+                up[j] = temp; 
+                break;
+            }
+        }
+    }
+
+    cout << "Upper=" << str << endl;
+
+    return 0;
 }
