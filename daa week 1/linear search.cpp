818dd1c95df8bdd6b27e487776e0d1a95cb93157
aditya anand <aditya68523490@gmail.com>
2025-08-19 08:26:13 +0530

    Update linear search.cpp

--- daa week 1/linear search.cpp
+++ daa week 1/linear search.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
+
 int main(){
     int arr[]={1,3,4,2,5,9,7,5,0};
     int n = sizeof(arr) / sizeof(arr[0]);
     int key;
     cin>>key;
+    
     for(int i=0;i<n;i++){
         if(arr[i]==key){
             cout<<"element found at"<<i+1<<endl;
@@ -14,4 +16,5 @@ int main(){
        
 
     }
+
 }
