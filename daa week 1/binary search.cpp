4e9d8f25a9fe1bad822e6b42ce4709a9bd0f6085
aditya anand <aditya68523490@gmail.com>
2025-08-19 08:20:31 +0530

    Update binary search.cpp

--- daa week 1/binary search.cpp
+++ daa week 1/binary search.cpp
@@ -5,26 +5,27 @@ int main() {
     int arr[] = {1,2,3,4,5,6,7,8,9};
     int key = 7;
     int n = sizeof(arr) / sizeof(arr[0]);
-    int low = 0, high = n - 1;
+    int l = 0, h = n - 1;
     bool found = false;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
+    while (l <= h) {
+        int mid = (l + h) / 2;
 
         if (arr[mid] == key) {
-            cout << "Element found at position " << mid + 1 << endl;
+            cout << "key position= " << mid + 1 << endl;
             found = true;
             break;
         } else if (arr[mid] < key) {
-            low = mid + 1;
+            l = mid + 1;
         } else {
-            high = mid - 1;
+            h = mid - 1;
         }
     }
 
     if (!found) {
-        cout << "Element not found in the array." << endl;
+        cout << "key not in array" << endl;
     }
 
     return 0;
+
 }
