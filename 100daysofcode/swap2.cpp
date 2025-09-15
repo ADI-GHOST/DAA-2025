65388094dcd767868daf51b4831dde6bad7e44d8
aditya anand <aditya68523490@gmail.com>
2025-09-15 22:57:36 +0530

    Create swap2.cpp

---
+++ 100daysofcode/swap2.cpp
@@ -0,0 +1,14 @@
+//Write a program to swap two numbers without using a third variable.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "After swap a=" << a << ", b = " << b << endl;
+    return 0;
+}
