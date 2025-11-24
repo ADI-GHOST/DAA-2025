7ebcc87e2b11f75514b8843328725a8550f535d1
aditya anand <aditya68523490@gmail.com>
2025-11-24 19:03:35 +0530

    Refactor KMP algorithm for clarity and consistency

--- daa week 5/kmp.cpp
+++ daa week 5/kmp.cpp
@@ -14,26 +14,39 @@ int main() {
 
     while (b < m) {
         if (p[b] == p[a]) {
-            a++;
+            a = a + 1;
             l[b] = a;
-            b++;
+            b = b + 1;
         } else {
-            if (a != 0) a = l[a - 1];
-            else { l[b] = 0; b++; }
+            if (a != 0)
+                a = l[a - 1];
+            else {
+                l[b] = 0;
+                b = b + 1;
+            }
         }
     }
 
     int i = 0, j = 0;
 
     while (i < n) {
-        if (t[i] == p[j]) { i++; j++; }
+
+        if (t[i] == p[j]) {
+            i = i + 1;
+            j = j + 1;
+        }
+
+        else if (t[i] != p[j]) {
+            if (j != 0)
+                j = l[j - 1];
+            else
+                i = i + 1;
+        }
 
         if (j == m) {
-            cout << (i - j) << "\n";
+            cout << i - j << "\n";
             j = l[j - 1];
-        } else if (i < n && t[i] != p[j]) {
-            if (j != 0) j = l[j - 1];
-            else i++;
         }
     }
 }
+
